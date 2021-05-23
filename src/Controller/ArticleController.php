4db0098d8741954fc2c500e7d98fe361ce238ae2
Mayssa Ayadi <maissa.ayadi.te@gmail.com>
2021-05-23 23:50:55 +0100

    fix erreur chapitre 5

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -33,6 +33,7 @@ class ArticleController extends AbstractController
             throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
         }
 
+        dump($article);die;
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
---
+++ src/Service/SlackClient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service;
+
+class SlackClient
+{
+
+
+   
+
+    public function sendMessage(string $from, string $message)
+    {
+       
+    }
+}
